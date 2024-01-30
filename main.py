c8b73b4c2767143fcce5fbe7be01fdc231cce190
Luis Lafore <lh946630@gmail.com>
2024-01-29 22:11:24 -0300

    esta quase funcionando

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class Termooo():
     #Metodo construtor, cria um dict vazio para as letras escolhidas e recebe a palavra do jogo
     def __init__(self,palavra):
         self.palavra = palavra
+        self.tentativas = 0
+        self.letras_certas = 0
         self.letras_escolhidas = {}
     #Mostra o progresso do jogo         
     def mostra_jogo(self):
@@ -32,13 +34,34 @@ class Termooo():
                 for letra_palavra in self.palavra:                    
                     if letra_tenativa == letra_palavra and indice_palavra == indice_tentativa:
                         self.letras_escolhidas[letra_tenativa] = indice_palavra
+                        self.letras_certas += 1
                     elif letra_tenativa == letra_palavra and indice_palavra != indice_tentativa:
                         self.letras_escolhidas[letra_tenativa] = 0
                     elif letra_tenativa != letra_palavra:
                         self.letras_escolhidas[letra_tenativa] = 'x'
                 indice_palavra += 1
             indice_tentativa += 1
-        mostra_jogo()
+            self.tentativas += 1
+        self.mostra_jogo()
     
-    def end_game():
-        pass
+    def end_game(self):
+        if self.tentativas >= 5 or self.letras_certas >= 5:
+            return False
+        else:
+            return True
+        
+def main():
+    possibilidades = ['olhos','pegar','lanca']
+    
+    jogo = random.choice(possibilidades)
+    
+    game = Termooo(jogo)
+    
+    while game.end_game():
+        tentou = input('Palavra: ')
+        game.tentativa(tentou)
+    
+    print("A palavra era ", jogo)
+    
+if __name__ == "__main__":
+	main()
